66e08674f7371340d9305483cb42f479dc72647a
Khushdeep Kaur <khushdeepk1706@gmail.com>
2026-06-21 22:32:26 -0400

    Created random number and computer selection variables

--- LAB02/lab02.js
+++ LAB02/lab02.js
@@ -4,5 +4,7 @@ prompt.start();
 
 prompt.get(['userSelection'], function (err, result) {
     let userSelection = result.userSelection.toUpperCase();
+    let randomNumber = Math.random();
+let computerSelection = "";
 
 });
