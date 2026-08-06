790eb5e7e30c71e00ff588ec8fa46eef8c8ea61c
Khushdeep Kaur <khushdeepk1706@gmail.com>
2026-08-06 02:51:09 -0400

    Add resource creation form and database insert

--- ASSIGNMENT02/routes/resources.js
+++ ASSIGNMENT02/routes/resources.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var Resource = require('../models/Resource');
 
 // Public directory page
-// Anyone can view resources, but this route does not allow editing or deleting.
 router.get('/', async function (req, res, next) {
   try {
     const resources = await Resource.find().sort({ createdAt: -1 });
@@ -15,6 +14,37 @@ router.get('/', async function (req, res, next) {
     });
   } catch (error) {
     next(error);
+  }
+});
+
+// Display the form for creating a new resource
+router.get('/new', function (req, res) {
+  res.render('resources/new', {
+    title: 'Add Resource'
+  });
+});
+
+// Save a new resource to MongoDB
+router.post('/', async function (req, res, next) {
+  try {
+    await Resource.create({
+      name: req.body.name,
+      category: req.body.category,
+      description: req.body.description,
+      address: req.body.address,
+      city: req.body.city,
+      phone: req.body.phone,
+      website: req.body.website,
+      isFree: req.body.isFree === 'on'
+    });
+
+    res.redirect('/resources');
+  } catch (error) {
+    res.status(400).render('resources/new', {
+      title: 'Add Resource',
+      errorMessage: error.message,
+      formData: req.body
+    });
   }
 });
 
